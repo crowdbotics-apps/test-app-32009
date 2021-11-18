136d3c5ac9c41b11a4f94afa78764bc923a245fe
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 04:34:42 -0700

    Update views.py

--- backend/question/views.py
+++ backend/question/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Answer, InputType, Option, Question, QuestionOption
 from django.http import JsonResponse
+from api/v1 import serializer
 
 # Create your views here.
 
 def user_profile(request):
-  user_data = Answer.objects.filter(user_id=1).values()#request.POST['user_id']
+  user_data = Answer.objects.filter(user_id=1)#request.POST['user_id']
+  user_data = AnswerSerializer(user_data).data
   print('user_data')
   print(user_data)
   print('type of : ', type(user_data))
