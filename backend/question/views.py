0ca6f8730d7147f79ade02fc3b9c625a327e6eb0
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 00:31:44 -0700

    Update views.py

--- backend/question/views.py
+++ backend/question/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_profile(request):
+  user_data = Answer.objects.filter(user_id=request.POST['user_id'])
+  return user_data
