42738e2ed0d52b2b799ea2c9cb406e030319b15d
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 00:18:57 -0700

    Update admin.py

--- backend/question/admin.py
+++ backend/question/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Answer, InputType, Option, Question, QuestionOption
 
-admin.site.register(Option)
+# admin.site.register(Option)
 admin.site.register(Question)
 admin.site.register(QuestionOption)
 admin.site.register(Answer)
 admin.site.register(InputType)
 
 # Register your models here.
+ModelField= lambda model: type('Subclass'+model.__name__,(admin.ModelAdmin,),{
+	'list_display':[x.name for x in model._meta.fields],	
+})
+
+admin.site.register(Option,ModelField(Option))
