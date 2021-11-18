bceede2bef8f8e55f430ad9638b8157c9243fd94
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 00:43:00 -0700

    Update urls.py

--- backend/question/api/v1/urls.py
+++ backend/question/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from question import views
 from .viewsets import (
     AnswerViewSet,
     InputTypeViewSet,
@@ -17,4 +18,5 @@ router.register("inputtype", InputTypeViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path('api/v1/profile/', views.user_profile),
 ]
