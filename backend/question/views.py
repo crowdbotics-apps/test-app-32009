959392f599b40ef9337a06f573f3b00e7527cfff
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 01:12:27 -0700

    Update views.py

--- backend/question/views.py
+++ backend/question/views.py
@@ -4,5 +4,6 @@ from .models import Answer, InputType, Option, Question, QuestionOption
 # Create your views here.
 
 def user_profile(request):
-  user_data = Answer.objects.filter(user_id=request.POST['user_id'])
+  user_data = Answer.objects.filter(user_id=1)#request.POST['user_id']
+  return {'name':'chirag'}
   return user_data
