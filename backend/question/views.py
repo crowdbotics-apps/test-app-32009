f163459703f2f675c757d3f4e584087be3900236
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 03:06:48 -0700

    Update views.py

--- backend/question/views.py
+++ backend/question/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Answer, InputType, Option, Question, QuestionOption
+from django.http import JsonResponse
 
 # Create your views here.
 
 def user_profile(request):
   user_data = Answer.objects.filter(user_id=1)#request.POST['user_id']
+  return JsonResponse({'foo':'bar'})
   return {'name':'chirag'}
   return user_data
