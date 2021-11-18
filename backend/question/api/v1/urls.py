63e5c8722952e362e9a44527190fbaf74611d849
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 01:27:28 -0700

    Update urls.py

--- backend/question/api/v1/urls.py
+++ backend/question/api/v1/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from question import views
 from . import views
-from ../../ import views
 from .viewsets import (
     AnswerViewSet,
     InputTypeViewSet,
