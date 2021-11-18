cd7a790463a50bc5a0c95efcadb369a868162311
chiragcrowdbotics <94012703+chiragcrowdbotics@users.noreply.github.com>
2021-11-18 04:19:28 -0700

    Update views.py

--- backend/question/views.py
+++ backend/question/views.py
@@ -8,5 +8,5 @@ def user_profile(request):
   user_data = Answer.objects.filter(user_id=1)#request.POST['user_id']
   print('user_data')
   print(user_data)
-  print('type of : ' + type(user_data))
+  print('type of : ', type(user_data))
   return JsonResponse(user_data)
